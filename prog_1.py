2f9a207fdbab39705ae726520509889984528e3b
Aaditya Sriram <aadisriram@gmail.com>
2015-10-01 22:42:51 -0400

    Adding part 1 of the project

---
+++ prog_1.py
@@ -0,0 +1,27 @@
+from snap import *
+
+nodes = [0, 107, 348, 414, 686,698, 1684, 1912, 3437, 3980]
+graph = TUNGraph.New()
+with open("./files/facebook_combined.txt") as combined_file:
+    for line in combined_file:
+        x = [int(i) for i in line.split()]
+        if not graph.IsNode(x[0]):
+            graph.AddNode(x[0])
+        if not graph.IsNode(x[1]):
+            graph.AddNode(x[1])
+        if not graph.IsEdge(x[0], x[1]):
+            graph.AddEdge(x[0], x[1])
+
+print graph.GetNodes()
+print graph.GetEdges()
+blah = TIntFltH()
+halb = TIntPrFltH()
+NodeFrac = float()
+GetBetweennessCentr(graph, blah, halb, 1.0)
+clust_coeff = GetClustCf(graph, -1)
+
+for node in nodes:
+    print("Betweeness centrality for %d is %f"%(node, blah[node]))
+print("Clustering coefficient %f"%clust_coeff)
+
+
