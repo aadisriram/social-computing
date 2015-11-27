b6f2e825bd163f5d57fc0205553bc75c5344c12f
Aaditya Sriram <aadisriram@gmail.com>
2015-11-27 18:25:04 -0500

    Basic data

--- project/index.js
+++ project/index.js
@@ -43,8 +43,65 @@ fs.readFile('group_members.json', 'utf8', function (err, data) {
   	if (error) throw error;
   	group_feed = JSON.parse(feed_data);
   	for (var i in group_feed.data) {
-  		var memberAction = member_map[group_feed.data[i].from.id];
-  		console.log(memberAction);
+  		// console.log(group_feed.data[i].from.id);
+  		var feed_entry = group_feed.data[i];
+  		var memberAction = member_map[feed_entry.from.id];
+  		if (memberAction) {
+  			memberAction.number_posts++;
+  			if (feed_entry.likes) {
+  				memberAction.number_likes_received += feed_entry.likes.data.length;
+  			}
+
+  			if (feed_entry.shares) {
+  				memberAction.number_got_shared_post += feed_entry.shares.count;
+  			}
+
+  			if (feed_entry.comments) {
+  				memberAction.number_comments_received += feed_entry.comments.data.length;
+  				var comments_data = feed_entry.comments.data;
+  				for (var k = 0; k < comments_data.length; k++) {
+  					if (comments_data[k].message_tags) {
+  						if (member_map[comments_data[k].from.id]) {
+  							member_map[comments_data[k].from.id].number_comments++;
+  							member_map[comments_data[k].from.id].comments_tagged_other += comments_data[k].message_tags.length;
+  							member_map[comments_data[k].from.id].number_likes_received += comments_data[k].like_count;
+  						}
+  						for (var j = 0; j < comments_data[k].message_tags.length; j++) {
+  							if (member_map[comments_data[k].message_tags[j].id]) {
+  								member_map[comments_data[k].message_tags[j].id].number_comment_tagged++;
+  							}
+  						}
+  					}
+  				}
+  			}
+
+  			if (feed_entry.shares) {
+  				memberAction.number_got_shared_post += feed_entry.shares;
+  			}
+
+  			if (feed_entry.link) {
+  				memberAction.number_external_url++;
+  			}
+
+  			if (feed_entry.story) {
+  				if (feed_entry.story.indexOf('file') > -1) {
+  					memberAction.files_uploaded++;
+  				}
+  			}
+
+  			if (feed_entry.to.data.length > 1)
+  			{
+  				memberAction.posts_tagged_other += feed_entry.to.data.length - 1;
+  				for (i = 1; i < feed_entry.to.data.length; i++) {
+  					if (member_map[feed_entry.to.data[i].id]) {
+  						member_map[feed_entry.to.data[i].id].number_posts_tagged++;
+  					}
+  				}
+  			}
+
+  			member_map[feed_entry.from.id] = memberAction;		
+  		}
   	}
+	console.log(member_map[4329673775529]);
   });
 });
